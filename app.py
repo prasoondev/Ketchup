d9d6a45f44e77850d5d9d576be866f28c5e40d5f
ArihantRastogi <143735797+ArihantRastogi@users.noreply.github.com>
2024-03-10 15:17:35 +0530

    Update app.py to move to psycopg2 as well have some error handling

--- app.py
+++ app.py
@@ -9,16 +9,12 @@ from mysql.connector import Error
 import base64
 import secrets
 import hashlib
+import os
+import psycopg2
+import random
 
 # Initialize Flask app
 app = Flask(__name__)
-
-# Configure MySQL database
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'hello'
-app.config['MYSQL_PASSWORD'] = '123'
-app.config['MYSQL_DB'] = 'project'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # Initialize MySQL and Bcrypt instances
 mysql = MySQL(app)
@@ -27,7 +23,40 @@ bcrypt = Bcrypt(app)
 # Generate a secret key for JWT
 secret_key = secrets.token_hex(32)
 app.secret_key = secret_key
-
+def create_table():
+    conn = connect_to_database()
+    with conn.cursor() as cur:
+# Define the SQL statement to create the table
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS UserDetails (
+            UserId SERIAL PRIMARY KEY,
+            UserName VARCHAR(255) NOT NULL UNIQUE,
+            UserEmail VARCHAR(255) UNIQUE,
+            UserPassword VARCHAR(255) NOT NULL
+        );
+        """
+        cur.execute(create_table_query)
+        conn.commit()
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS UserImages (
+            ImageId SERIAL PRIMARY KEY,
+            UserId INT, 
+            ImageData BYTEA,
+            ImageMetadata VARCHAR(255)
+        );
+        """
+        cur.execute(create_table_query)
+        conn.commit()
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS Audio (
+            AudioID SERIAL PRIMARY KEY,
+            AudioBlob BYTEA,
+            AudioMetadata TEXT
+        );
+        """
+        cur.execute(create_table_query)
+        conn.commit()
+        conn.close()
 # Define a decorator to require authentication
 def token_required(f):
     @wraps(f)
@@ -48,9 +77,29 @@ def token_required(f):
 
     return decorated_function
 
+def connect_to_database():
+    conn_params = {
+        'host': 'course-project-group-55-4059.7s5.aws-ap-south-1.cockroachlabs.cloud',
+        'port': 26257,
+        'user': 'Ketchup',
+        'password': 'F1wKiMCuYXXiIiUsCzbsOg',
+        'database': 'project',
+        'sslmode': 'verify-full',
+        'sslrootcert': 'root.crt' # Replace with the correct path
+    }
+
+    conn_str = "host={host} port={port} user={user} password={password} dbname={database} sslmode={sslmode} sslrootcert={sslrootcert}".format(**conn_params)
+
+    # Connect to the database
+    try:
+        conn = psycopg2.connect(conn_str)
+        return conn
+    except psycopg2.OperationalError as e:
+        return None
 
 @app.route("/")
 def form():
+    create_table()
     token = request.cookies.get('token')
     if token:
         try:
@@ -64,14 +113,16 @@ def form():
         except jwt.InvalidTokenError:
             # Invalid token, render the registration page
             pass
-    cur = mysql.connection.cursor()
+    conn = connect_to_database()
+    cur = conn.cursor()
     cur.execute("SELECT * FROM UserDetails WHERE UserName = %s", ('admin',))
     admin_user = cur.fetchone()
     if not admin_user:
         hashed_password = bcrypt.generate_password_hash('123456').decode('utf-8')
         cur.execute("INSERT INTO UserDetails (UserName, UserPassword) VALUES (%s, %s)", ('admin', hashed_password))
-    mysql.connection.commit()
+    conn.commit()
     cur.close()
+    conn.close()
     return render_template("register.html")
 
 @app.route("/register", methods=["GET", "POST"])
@@ -81,10 +132,12 @@ def register():
         password = request.form['register-password']
         email = request.form['register-useremail']
         
-        cur = mysql.connection.cursor()
+        conn = connect_to_database()
+        cur = conn.cursor()
         cur.execute("SELECT * FROM UserDetails WHERE UserName = %s", (username,))
         user = cur.fetchone()
         cur.close()
+        conn.close()
 
         if user:
             return render_template('register.html', error='User Email already exists')
@@ -92,11 +145,12 @@ def register():
         # Hash the password before storing it
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
-        cur = mysql.connection.cursor()
+        conn = connect_to_database()
+        cur = conn.cursor()
         cur.execute("INSERT INTO UserDetails (UserName, UserEmail, UserPassword) VALUES (%s, %s, %s)", (username, email, hashed_password))
-        mysql.connection.commit()
+        conn.commit()
         cur.close()
-
+        conn.close()
         flash('Registration successful. Please log in.', 'success')
         return redirect(url_for('login'))
     
@@ -110,16 +164,18 @@ def admin():
         username = data['login-username']
         password = data['login-password']
         if username == 'admin':
-            cur = mysql.connection.cursor()
+            conn = connect_to_database()
+            cur = conn.cursor()
             cur.execute("SELECT * FROM UserDetails WHERE UserName = %s", (username,))
             admin_user = cur.fetchone()
             cur.close()
             if admin_user and bcrypt.check_password_hash(admin_user['UserPassword'], password):
-                cur = mysql.connection.cursor()
+                conn = connect_to_database()
+                cur = conn.cursor()
                 cur.execute("SELECT * FROM UserDetails")
                 user_details = cur.fetchall()
                 cur.close()
-
+                conn.close()
                 # Set cookie for the username
                 response = make_response(render_template('admin.html', user_details=user_details))
                 response.set_cookie('username', username)
@@ -139,12 +195,14 @@ def login():
         username = request.form['login-username']
         password = request.form['login-password']
         
-        cur = mysql.connection.cursor()
+        conn = connect_to_database()
+        cur = conn.cursor()
         cur.execute("SELECT * FROM UserDetails WHERE UserName = %s", (username,))
         user = cur.fetchone()
         cur.close()
+        conn.close()
 
-        if user and bcrypt.check_password_hash(user['UserPassword'], password):
+        if user and bcrypt.check_password_hash(user[3], password):
             # Generate JWT token
             token = jwt.encode({'username': username}, secret_key, algorithm='HS256')
 
@@ -166,42 +224,45 @@ def login():
 @token_required
 def website(current_user):
     # Retrieve user_id based on username
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT UserId FROM UserDetails WHERE Username = %s", (current_user,))
-    user = cursor.fetchone()
+    conn = connect_to_database()
+    cur = conn.cursor()
+    cur.execute("SELECT UserId FROM UserDetails WHERE Username = %s", (current_user,))
+    user = cur.fetchone()
 
     if not user:
         return render_template('website.html', message="User not found.")
 
-    user_id = user['UserId']
+    user_id = user[0]
 
     # Retrieve images uploaded by the user
-    cursor.execute("SELECT * FROM UserImages WHERE UserId = %s", (user_id,))
-    images = cursor.fetchall()
+    cur.execute("SELECT * FROM UserImages WHERE UserId = %s", (user_id,))
+    images = cur.fetchall()
 
     encoded_images = []
     for image in images:
-        encoded_image = base64.b64encode(image['ImageData']).decode('utf-8')
+        encoded_image = base64.b64encode(image[2]).decode('utf-8')
         encoded_images.append(encoded_image)
 
     if not encoded_images:
         # If no images are found, render the website with a message
         return render_template('website.html', message="No images found for the user.")
-
+    cur.close()
+    conn.close()
     return render_template('website.html', images=encoded_images)
 
 @app.route('/videomaker')
 @token_required
 def videomaker(current_user):
     # Retrieve user_id based on username
-    cursor = mysql.connection.cursor()
+    conn = connect_to_database()
+    cursor = conn.cursor()
     cursor.execute("SELECT UserId FROM UserDetails WHERE Username = %s", (current_user,))
     user = cursor.fetchone()
 
     if not user:
         return render_template('videomaker.html', message="User not found.")
 
-    user_id = user['UserId']
+    user_id = user[0]
 
     # Retrieve images uploaded by the user
     cursor.execute("SELECT * FROM UserImages WHERE UserId = %s", (user_id,))
@@ -209,13 +270,14 @@ def videomaker(current_user):
 
     encoded_images = []
     for image in images:
-        encoded_image = base64.b64encode(image['ImageData']).decode('utf-8')
+        encoded_image = base64.b64encode(image[2]).decode('utf-8')
         encoded_images.append(encoded_image)
 
     if not encoded_images:
         # If no images are found, render the website with a message
         return render_template('videomaker.html', message="No images found for the user.")
-
+    cursor.close()
+    conn.close()
     return render_template('videomaker.html', images=encoded_images)
 
 
@@ -231,15 +293,16 @@ def upload_page(current_user):
         files = request.files.getlist('files')
 
         # Retrieve user_id based on username
-        cursor = mysql.connection.cursor()
+        conn = connect_to_database()
+        cursor = conn.cursor()
         cursor.execute("SELECT UserId FROM UserDetails WHERE UserName = %s", (current_user,))
         user = cursor.fetchone()
 
         if not user:
             return 'User not found.'
 
-        user_id = user['UserId']
-
+        user_id = user[0]
+        flag=0
         for file in files:
             if file.filename == '':
                 continue  # Skip empty file inputs
@@ -248,7 +311,6 @@ def upload_page(current_user):
             image_data = file.read()
             combined_data = f"{user_id}:{image_data}".encode('utf-8')
             image_hash = hashlib.sha256(combined_data).hexdigest()
-
             try:
                 # Check if the image hash already exists in ImageMetadata column of UserImages table
                 cursor.execute("SELECT * FROM UserImages WHERE ImageMetadata = %s", (image_hash,))
@@ -256,18 +318,24 @@ def upload_page(current_user):
 
                 if existing_image:
                     flash('You have already uploaded one or more images.', 'error')
+                    flag=1
                     continue
 
                 # Insert image data and metadata into UserImages table
                 cursor.execute("INSERT INTO UserImages (UserId, ImageMetadata, ImageData) VALUES (%s, %s, %s)", (user_id, image_hash, image_data))
-                mysql.connection.commit()
-
+                conn.commit()
             except Error as e:
+                msg = 'Error occurred! Please Try Again.'
                 print(f"The error '{e}' occurred")
                 flash('An error occurred while uploading one or more images.', 'error')
-
-        return redirect('/upload')
-
+                return render_template('upload.html', msg=msg)
+        cursor.close()
+        conn.close()
+        if(flag):
+            msg = 'Some Images were already in Database'
+        else:
+            msg = 'Uploaded Successfully'
+        return render_template('upload.html', msg=msg)
     return render_template('upload.html')
 
 @app.route('/delete_images', methods=['GET', 'POST'])
@@ -280,11 +348,13 @@ def delete_images(current_user):
         if images_to_delete:
             try:
                 # Delete the selected images from the database
-                cursor = mysql.connection.cursor()
+                conn = connect_to_database()
+                cursor = conn.cursor()
                 for image_id in images_to_delete:
                     cursor.execute("DELETE FROM UserImages WHERE ImageId = %s AND UserId = (SELECT UserId FROM UserDetails WHERE UserName = %s)", (image_id, current_user))
-                    mysql.connection.commit()
-
+                    conn.commit()
+                cursor.close()
+                conn.close()
                 flash('Selected images deleted successfully.', 'success')
             except Error as e:
                 print(f"The error '{e}' occurred")
@@ -293,15 +363,22 @@ def delete_images(current_user):
             return redirect('/delete_images')
 
     # Fetch images associated with the current user from the database
-    cursor = mysql.connection.cursor()
+    conn = connect_to_database()
+    cursor = conn.cursor()
     cursor.execute("SELECT * FROM UserImages WHERE UserId = (SELECT UserId FROM UserDetails WHERE UserName = %s)", (current_user,))
     images = cursor.fetchall()
     cursor.close()
+    conn.close()
+    modified_images = []
     for image in images:
-        image_data = base64.b64encode(image['ImageData']).decode('utf-8')
-        image['ImageData'] = image_data
+        image_data = base64.b64encode(image[2]).decode('utf-8')
+        modified_images.append({
+            'ImageId': image[0],
+            'ImageData': image_data,
+            'ImageMetadata': image[3]  # Assuming ImageMetadata is the fourth column
+        })
 
-    return render_template('delete_images.html', images=images)
+    return render_template('delete_images.html', images=modified_images)
 
 
 @app.route('/logout', methods=['GET', 'POST'])
